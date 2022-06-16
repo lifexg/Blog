16c77d48ae00900e9e8e1a213c7a2fa2b18b729a
fexg <frank@lifexg.com>
2022-06-16 17:30:35 +0800

    Add files via upload
    origin

---
+++ Blog/Blog/BlogApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlogApp.swift
+//  Blog
+//
+//  Created by fexg on 2022/6/16.
+//
+
+import SwiftUI
+
+@main
+struct BlogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
